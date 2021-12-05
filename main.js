34e11b5e6741e3c2bb0ead671f418d80ee8a46af
SAR-KUMAR <88149210+SAR-KUMAR@users.noreply.github.com>
2021-12-05 11:39:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var names = ["Fmaily Book","Dr.Jaya Gupta(mom)","Dr.Alok Kumar(dad)","Sarthak Kumar(me)"];
+var i = 0
+function update()
+{
+  document.getElementById("names").innerHTML = names[i];
+  i++;
+
+}
+{
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
